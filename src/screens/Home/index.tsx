28a3f933f1d49a33cc9b370d547c0af01865a472
unknown <gvmertens@gmail.com>
2023-06-23 14:36:28 -0300

    20230623

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -17,17 +17,19 @@ export function Home() {
         Quarta, 21 de junho de 2023
       </Text>
 
-      <TextInput 
-        style={styles.input}
-        placeholder='Nome do participante'
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>
-          +
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
